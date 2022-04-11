b74bc2d59a536dcf5b9ca3a6596ae8d009f0766d
Michal Szabados <misko.sz@gmail.com>
2022-04-11 15:19:06 +0200

    Add deaths plot

--- covid_weekly.py
+++ covid_weekly.py
@@ -9,23 +9,23 @@ import matplotlib.ticker as ticker
 
 class Dataset:
     confirmed: pd.DataFrame
+    deaths: pd.DataFrame
     territories: Dict[str, Dict[str, Any]]
 
     def __init__(self):
         self.confirmed = pd.read_csv("time_series_covid19_confirmed_global.csv").fillna(value={"Province/State": "NULL"})
+        self.deaths = pd.read_csv("time_series_covid19_deaths_global.csv").fillna(value={"Province/State": "NULL"})
         territories_raw = pd.read_csv("UID_ISO_FIPS_LookUp_Table.csv").fillna(value={"Province_State": "NULL"})
         self.territories = territories_raw.set_index('Combined_Key').T.to_dict()
 
 
-def _territory_confirmed_per100k(dataset: Dataset, territory_key: str) -> pd.Series:
-    territory = dataset.territories[territory_key]
+def _territory_total_per100k(series: pd.Series, territory: Dict[str, Any]) -> pd.Series:
     population = territory["Population"]
-
-    selector_confirmed = (
-        (dataset.confirmed["Country/Region"] == territory["Country_Region"]) & \
-        (dataset.confirmed["Province/State"] == territory["Province_State"])
+    selector = (
+        (series["Country/Region"] == territory["Country_Region"]) & \
+        (series["Province/State"] == territory["Province_State"])
     )
-    row = dataset.confirmed[selector_confirmed]
+    row = series[selector]
     # Start displaying data from the week following Sunday Feb 23.
     total = row.loc[:,"2/23/20":].squeeze()
     return total / population * 100000
@@ -54,7 +54,7 @@ def _date_fmt(date_str: str):
     return dt.strftime("%b")
 
 
-def _set_yticks(ax: matplotlib.axis.Axis, ymax: int, ytickstep: int):
+def _set_yticks(ax: matplotlib.axis.Axis, ymax: float, ytickstep: float):
     ax.set_ylim([0, ymax])
     ax.set_yticks(np.arange(ytickstep, ymax + 1, ytickstep))
     ax.yaxis.grid(True)
@@ -84,35 +84,46 @@ def _set_xticks(ax: matplotlib.axis.Axis, series: pd.Series):
 def _bar_plot(ax: matplotlib.axis.Axis, series: pd.Series, color: str) -> None:
     x = [_date_to_days(pd.Timestamp(d)) for d in series.index]
     y = list(series)
-    ax.bar(x, y, width=6)
+    ax.bar(x, y, width=6, color=color)
 
 
 def territory_plot(dataset: Dataset, territory_key: str) -> matplotlib.figure.Figure:
-    total = _territory_confirmed_per100k(dataset=dataset, territory_key=territory_key)
-    weekly = _weekly_series(total=total)
-
     plt.rcParams["font.family"] = "serif"
     plt.rcParams["font.serif"] = ["charter", "Georgia", "Cambria", "Times New Roman", "Times", "serif"]
     plt.rcParams["font.size"] = 11
 
-    # figure = plt.figure(figsize=(10, 8))
-    # spec = matplotlib.gridspec.GridSpec(nrows=2, ncols=1, height_ratios=[2, 1])
+    figure = plt.figure(figsize=(10, 8))
+    spec = matplotlib.gridspec.GridSpec(nrows=2, ncols=1, height_ratios=[3, 2])
 
-    # ax0 = figure.add_subplot(spec[0])
-    figure = plt.figure(figsize=(10, 5))
-    ax0 = figure.add_subplot(111)
+    # Cases
+    cases_total = _territory_total_per100k(
+        series=dataset.confirmed,
+        territory=dataset.territories[territory_key],
+    )
+    weekly = _weekly_series(total=cases_total)
+
+    ax0 = figure.add_subplot(spec[0])
+    # figure = plt.figure(figsize=(10, 5))
+    # ax0 = figure.add_subplot(111)
     _bar_plot(ax0, weekly, "steelblue")
     _set_yticks(ax0, ymax=3000, ytickstep=250)
     _set_xticks(ax0, weekly)
     ax0.set_title(f"New cases weekly per 100k inhabitants (current: {weekly[-1]:.0f})", pad=12)
 
-    # ax1 = figure.add_subplot(spec[1])
-    # _bar_plot(ax=ax1, series=rate, color="steelblue")  # tab:blue slategrey
-    # ax1.set_ylim([-1.0, 2.0])
-    # ax1.yaxis.set_major_formatter(ticker.FuncFormatter(_rate_fmt))
-    # ax1.set_title(f"Week-over-week percent increase (current: {_rate_fmt(rate[-1])})", pad=12)
+    # Deaths
+    deaths_total = _territory_total_per100k(
+        series=dataset.deaths,
+        territory=dataset.territories[territory_key],
+    )
+    weekly = _weekly_series(total=deaths_total)
 
-    figure.suptitle(territory_key, fontsize=20, y=0.93)
+    ax1 = figure.add_subplot(spec[1])
+    _bar_plot(ax1, weekly, "slategray")
+    _set_yticks(ax1, ymax=15, ytickstep=2.5)
+    _set_xticks(ax1, weekly)
+    ax1.set_title(f"New deaths weekly per 100k inhabitants (current: {weekly[-1]:.1f})", pad=12)
+
+    figure.suptitle(territory_key, fontsize=20, y=0.95)
     figure.tight_layout(pad=1.8)
     figure.patch.set_facecolor('white')
     return figure
--- download_data.sh
+++ download_data.sh
@@ -1,2 +1,3 @@
 wget -O time_series_covid19_confirmed_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
+wget -O time_series_covid19_deaths_global.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
 wget -O UID_ISO_FIPS_LookUp_Table.csv https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv
